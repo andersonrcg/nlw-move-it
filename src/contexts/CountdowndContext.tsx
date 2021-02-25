d06118ad5a45d957f0192e47b20246799b4a555b
Anderson Gabriel <anderson.rcg@gmail.com>
2021-02-25 12:41:53 -0300

    Dia 04

---
+++ src/contexts/CountdowndContext.tsx
@@ -0,0 +1,68 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { ChallengesContext } from "./ChallengeContext";
+
+interface CountdownContextData {
+    minutes: number;
+    seconds: number;
+    hasFinished: boolean;
+    isActive: boolean;
+    startCountdown: () => void;
+    resetCountdown: () => void;
+}
+
+interface CountdownProviderProps {
+    children: ReactNode;
+}
+
+export const CountdownContext = createContext({} as CountdownContextData);
+
+let countdownTimeout: NodeJS.Timeout;
+const durationChallenge = 0.04 * 60;
+
+export function CountdownProvider({ children }: CountdownProviderProps) {
+    const { startNewChallenge } = useContext(ChallengesContext);
+
+    const [time, setTime] = useState(durationChallenge);
+    const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
+
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+
+    function startCountdown() {
+        setIsActive(true);
+    }
+
+    function resetCountdown() {
+        clearTimeout(countdownTimeout);
+        setIsActive(false);
+        setHasFinished(false);
+        setTime(durationChallenge);
+    }
+
+    useEffect(() => {
+        console.log(time)
+        if(isActive && time >= 1) {
+            countdownTimeout = setTimeout(() => {
+                setTime(time - 1);
+            }, 1000)
+        } else if( isActive && time < 1){
+            setHasFinished(true);
+            setIsActive(false);
+            startNewChallenge();
+        }
+    }, [isActive, time])
+
+    return(
+        <CountdownContext.Provider value={{
+            minutes,
+            seconds,
+            hasFinished,
+            isActive,
+            startCountdown,
+            resetCountdown
+        }}>
+            {children}
+        </CountdownContext.Provider>
+    );
+}
